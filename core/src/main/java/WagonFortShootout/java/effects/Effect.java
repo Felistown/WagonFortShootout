0e49c720fdd4b2aabd46d50387f3bacca6af2df2
Felis <audricskyegaspar@gmail.com>
2026-02-26 22:45:17 -0800

    Rewrote ray - became beam Created Effect class

---
+++ core/src/main/java/WagonFortShootout/java/effects/Effect.java
@@ -0,0 +1,44 @@
+package WagonFortShootout.java.effects;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+
+import java.util.ArrayList;
+
+public class Effect {
+
+    private static final ArrayList<Effect> ALL_EFFECTS = new ArrayList<Effect>();
+
+    private final Sprite SPRITE;
+    private int lifetime;
+
+    public static void addEffect(Effect effect, int lifeTime) {
+        effect.lifetime = lifeTime;
+        ALL_EFFECTS.add(effect);
+    }
+
+    public Effect(Texture texture, float length, float height, Vector2 pos, float angle) {
+        SPRITE = new Sprite(texture);
+        SPRITE.setSize(length, height);
+        SPRITE.rotate(angle);
+        SPRITE.setCenter(pos.x, pos.y);
+    }
+
+    public static void renderAll(SpriteBatch spriteBatch) {
+        for(int i = 0; i < ALL_EFFECTS.size(); i ++) {
+            Effect r = ALL_EFFECTS.get(i);
+            r.render(spriteBatch);
+            if(r.lifetime <= 0) {
+                ALL_EFFECTS.remove(r);
+                i--;
+            }
+        }
+    }
+
+    public void render(SpriteBatch spriteBatch) {
+        SPRITE.draw(spriteBatch);
+        lifetime --;
+    }
+}
